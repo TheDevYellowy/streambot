ede41a4e585f4a23be0fe15e4a63a47e8c91ad33
Echo-BotDev <yellowymodz@gmail.com>
2021-03-29 15:21:54 -0500

    add eval command

---
+++ commands/Owner/eval.js
@@ -0,0 +1,44 @@
+const Command = require("../../base/Command.js");
+
+module.exports = class Eval extends Command {
+
+	constructor (client) {
+		super(client, {
+			name: "eval",
+			dirname: __dirname,
+			enabled: true,
+			aliases: [ "e" ],
+			memberPermissions: [],
+			botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
+			ownerOnly: true
+		});
+	}
+
+	async run (message, args, data) {
+        const usersData = this.client.usersData;
+        const guildsData = this.client.guildsData;
+
+        const content = message.content.split(" ").slice(1).join(" ");
+        const result = new Promise((resolve) => resolve(eval(content)));
+
+        return result.then((output) => {
+            if(typeof output !== "string"){
+                output = require("util").inspect(output, { depth: 0 });
+            }
+            if(output.includes(this.client.token)){
+                output = output.replace(this.client.token, "T0K3N");
+            }
+            message.channel.send(output, {
+                code: "js"
+            });
+        }).catch((err) => {
+            err = err.toString();
+            if(err.includes(this.client.token)){
+                err = err.replace(this.client.token, "T0K3N");
+            }
+            message.channel.send(err, {
+                code: "js"
+            });
+        });
+    }
+}
